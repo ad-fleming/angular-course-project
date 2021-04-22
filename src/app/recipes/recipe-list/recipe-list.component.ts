56d2edc7f349386e62ac780300c63fa4afef9a81
ad-fleming <adfleming9@gmail.com>
2021-04-22 11:37:48 -0400

    practicing using ngFor, string interpolation and property binding to display dummy data from our Recipe List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is just a test!', 'https://yournorthcounty.com/wp-content/uploads/2017/09/best-burger-in-san-diego-north-county-1024x647.jpg'),
+    new Recipe('Test Recipe', 'This is just a test!', 'https://yournorthcounty.com/wp-content/uploads/2017/09/best-burger-in-san-diego-north-county-1024x647.jpg')
+
+  ];
 
 
   constructor() { }
